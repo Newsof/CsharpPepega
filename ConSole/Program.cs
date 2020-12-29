7f1f8cf29a5761b37db844e8ba7fe3f3c7a85395
Newsof <Newsof@github.com>
2020-12-29 22:09:45 +0300

    PepeGA

---
+++ ConSole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyLib;
+
+namespace ConSole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine e = new Engine("Belarus", "AKAB228", EngineType.Diesel);
+            Cabin c = new Cabin("Cyberpunk", "A1077", CabinType.Tropic);
+            Tractor t = new Tractor("Naruto", "Saske", e, c);
+            Console.WriteLine(t.Serialize());
+        }
+    }
+}
